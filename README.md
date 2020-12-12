cef992b0801a3ff75ff69ee20f95526afdb9c139
Auddity <audsecondarylap@outlook.com>
2020-12-12 12:22:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Solution for Huddle Landing Page Challenge
+
+Challenge by [FrontendMentor](https://www.frontendmentor.io)
+
+Challenge completed using **Sass**
+
+I also added some transitions triggered with **JavaScript** when the page loads.
---
+++ script.js
@@ -0,0 +1,12 @@
+const header = document.getElementById('header');
+const mainImage = document.getElementById('mainImage');
+const mainContent = document.getElementById('mainContent');
+const footer = document.getElementById('footer');
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  header.style.transform = 'translateY(0)';
+  mainImage.style.transform = 'translateX(0)';
+  mainContent.style.transform = 'translateX(0)';
+  footer.style.transform = 'translateY(0)';
+});
